23ec7fec2480d3953c8da396b66815f448ab0350
alinealien <alinebdantunes@gmail.com>
2022-03-27 20:21:28 -0300

    Corrigindo Atividade I: Questão II

--- src/atividade1/SegundaQuestao.java
+++ src/atividade1/SegundaQuestao.java
@@ -1,11 +1,12 @@
 package atividade1;
 import javax.swing.*;
+import java.sql.Array;
 import java.util.*;
 
 /*2.Desenvolva um algoritmo que leia 10 números inteiros e os armazene sequencialmente em
 uma lista. Após isso, o programa deve:
 
-● Imprimir todos os elementos; OK
+● Imprimir todos os elementos;
 ● Imprimir somente os elementos pares;
 ● Imprimir e remover somente os elementos ímpares;
 ● Imprimir a soma dos elementos da lista.
@@ -13,35 +14,8 @@ uma lista. Após isso, o programa deve:
  */
 
 public class SegundaQuestao {
+public static void main(String[] args) {
 
-    private static Scanner scan;
-
-    public static void main(String[] args) {
-
-        scan = new Scanner(System.in);
-        ArrayList<Integer> ListaNumeros = new ArrayList<Integer>(10);
-
-        int i;
-        for (i = 0; i < 10; i++) {
-            Numeros numeros = new Numeros();
-            System.out.println("Digite um numero inteiro:");
-           numeros.ListaNumeros = scan.nextInt();
-
-        }
-        mostrar(ListaNumeros);
-    }
-        public static void mostrar(ArrayList<Integer> lista ){
-        int i, n = lista.size();
-
-        for(i=0; i < lista.size(); i++){
-            System.out.println(lista.get(i));
-
-            Arrays.toString(lista.toArray());
-        }
-
-    }
-}
-        /*
         Scanner ler = new Scanner(System.in);
 
         int numeros[] = new int[10]; //Vetor de 10 posições
@@ -63,7 +37,45 @@ public class SegundaQuestao {
 
 
             }
-        }*/
+        }
+
+
+        /*
+    private static Scanner scan;
+
+    public static void main(String[] args) {
+
+        List numerosAll;
+        scan = new Scanner(System.in);
+
+        ArrayList<Integer> ListaNumeros = new ArrayList<Integer>(10);
+
+        int i;
+        for (i = 0; i < 10; i++) {
+            Numeros numeros = new Numeros();
+            System.out.println("Digite um numero inteiro:");
+            numeros.ListaNumeros = scan.nextInt();
+
+        }
+        mostrar(ListaNumeros);
+
+    }
+
+    public static <ListaNumeros> void mostrar(ArrayList<Integer> lista) {
+        int i, n = lista.size();
+
+        for (i = 0; i < lista.size(); i++) {
+            System.out.println(lista.get(i));
+
+
+
+        }
+
+    }
+
+}*/
+
+
 
 
 
