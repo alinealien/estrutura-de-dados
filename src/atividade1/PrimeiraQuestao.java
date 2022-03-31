10472c617945eb7ab2d62a5e0a5cfbcc5817765b
alinealien <alinebdantunes@gmail.com>
2022-03-31 15:12:25 -0300

    Atualizando a questão 2 da atividade online I

--- src/atividade1/PrimeiraQuestao.java
+++ src/atividade1/PrimeiraQuestao.java
@@ -14,141 +14,33 @@ A recursão é utilizada quando queremos resolver um subproblema do mesmo tipo m
 
 public class PrimeiraQuestao {
 
-    // PROBLEMAS NA RESOLUÇÃO
-    // FAREI POR ULTIMO
+    public static void main(String[] args) {
 
+        Scanner numero = new Scanner(System.in);
 
+        System.out.println(" Digite um numero: ");
+        int resultado = somatorio (numero.nextInt());
 
+        System.out.println(resultado);
 
-}
+        int num = 0, i = 0;
+        int somatorio;
 
-    /*
-    public static void main(String[] args){
-
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um numero inteiro:");
-
-        PrimeiraQuestao cont = new PrimeiraQuestao();
-
-    }
-
-    private void imprimir(int i) {
-
-        if(i == 0){
-            System.out.println(0);
-    } else {
-        System.out.println( i + (i -1));
-    }
-        System.out.println(i + (i - 1));
-
-    }
-}
-    /*
-
-    // Método que imprime uma sequencia de números na ordem decrescente [x .. 1].
-    public void numeroSequencia(int x) {
-
-    /* Quando o valor de x for igual a 0 (zero), para a chamada do método
-       recursivamente.
-        if (x == 0) {
-            System.out.println(x + (x--));
-        } else {
-            // Imprime o valor de x.
-            System.out.println(x);
-            // Chama recursivamente este mesmo método passando o valor de x menos 1.
 
 
         }
-    }
 
-    public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um numero inteiro:");
-        int numero = scan.nextInt();
-
-        System.out.println();
-        //System.out.println(x + (numero - 1);
+    static int somatorio(int numero) {
 
         if (numero == 0) {
-            System.out.println(numero + (numero--));
-        } else {
-            // Imprime o valor de x.
-            System.out.println(numero);
-            // Chama recursivamente este mesmo método passando o valor de x menos 1.
-
-
-        }
-
-    }
-}
-
-
-    /*
-    public static void main(String[] args) {
-        int soma;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Digite um numero:");
-        int numero = scan.nextInt();
-
-        if (numero == 0){
-            System.out.println(0);
-        }else{
-            System.out.println(soma = (numero - 1) + numero);
-        }
-
-
-
-        //System.out.println("Digite um numero:");
-        //int n = scan.nextInt(); //recebe numero inteiro
-        //System.out.println("__________________________");
-
-    }
-
-
-}
-        /*
-        for (int somatorio = n; somatorio >= 0; somatorio--) {
-
-            System.out.println(somatorio);
-            System.out.println();
-
-            //System.out.println("__________________________");
-            System.out.println(soma += somatorio);
-
-        }
-    }
-}
-
-        /* Fibonacci - teste - OK
-        Scanner scan = new Scanner(System.in);
-
-
-        System.out.println("Digite um numero:");
-        int n = scan.nextInt(); //recebe numero inteiro
-
-        int somatorio = 1;
-
-        System.out.println(n + "! = ");
-        for(int i = n; i >= 1; i--) {
-            System.out.println(somatorio);
-            somatorio = somatorio * i;
-
-        }
-
-        System.out.println(somatorio);
-
-
-         public int somatorioNumeros(int soma) {
-
-        int nu = 0;
-
-        if (nu == 0) {
             return 0;
         } else {
-            return soma = nu + (nu++);
+            return numero + somatorio(numero - 1);
         }
-    }*/
 
+
+    }
+}
 
 
