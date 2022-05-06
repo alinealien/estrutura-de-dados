5cef19a2c9ed79620362bcf33288a8e08b4cbe20
alinealien <alinebdantunes@gmail.com>
2022-05-06 17:05:13 -0300

    Atualizando exercicio I - Busca Sequencial

--- UnidadeII/src/exercicios/BuscaSequencial.java
+++ UnidadeII/src/exercicios/BuscaSequencial.java
@@ -1,4 +1,53 @@
 package exercicios;
 
+import java.io.ByteArrayInputStream;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class BuscaSequencial {
+
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+
+        int[] v = gerarVetor(10);
+        System.out.println(Arrays.toString(v));
+
+
+
+        System.out.println("Digite um numero para pesquisa:");
+        int num = leitor.nextInt();
+
+        if(num == 34){
+            System.out.println("Achei!");
+        }
+
+
+    }
+
+    public static void imprimir(int v){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite um numero para pesquisa:");
+        int num = leitor.nextInt();
+
+        while(num == v){
+            System.out.println("Achei!");
+        }
+
+    }
+
+    public static int[] gerarVetor(int tamanho) {
+        int[] vetor = new int[tamanho];
+        Random gerador = new Random();
+
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = gerador.nextInt(100);
+        }
+
+        return vetor;
+    }
+
+
+
 }
