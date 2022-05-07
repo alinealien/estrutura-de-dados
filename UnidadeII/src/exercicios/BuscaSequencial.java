466f13e6f504de43f759f2b68653d2a7bdb4d630
alinealien <alinebdantunes@gmail.com>
2022-05-07 11:33:40 -0300

    Correção exercicio BuscaSequencial

--- UnidadeII/src/exercicios/BuscaSequencial.java
+++ UnidadeII/src/exercicios/BuscaSequencial.java
@@ -7,30 +7,33 @@ import java.util.Scanner;
 public class BuscaSequencial {
     public static void main(String[] args) {
 
-    Scanner leitor = new Scanner(System.in);
+        Scanner leitor = new Scanner(System.in);
 
-    //Aqui ele mostra o vetor com 10 numeros aleatorios
-    int[] v = gerarVetor(10);
-    System.out.println(Arrays.toString(v));
+        //Aqui ele mostra o vetor com 10 numeros aleatorios
+        int[] v = gerarVetor(10);
+        System.out.println(Arrays.toString(v));
 
 
-    //Aqui ele vai receber um numero para pesquisar no vetor
+        //Aqui ele vai receber um numero para pesquisar no vetor
         System.out.println("Digite um numero para pesquisa:");
         int num = leitor.nextInt();
 
 
-    //Um laço para fazer a verificação se o numero pesquisado esta ou não no vetor
-    for(int i = 0; i < v.length;i++){
-        if (v[i] == num) {
-            System.out.println("Achei " + num + " na posição " + i);
-            break;
-        } else if (i == v.length - 1) {
+        //Um laço para fazer a verificação se o numero pesquisado esta ou não no vetor
+        int i;
+        for (i = 0; i < v.length; i++) {
+            if (v[i] == num) {
+                System.out.println("Achei " + num + " na posição " + i);
+                break;
+            }
+
+        }
+
+        if (i == v.length - 1) {
             System.out.println("Número não encontrado!");
         }
 
     }
-
-}
    //Metodo para gerar os vetores aleatorios
     public static int[] gerarVetor(int tamanho) {
         int[] vetor = new int[tamanho];
